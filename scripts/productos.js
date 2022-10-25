2a5425383e02750e1fd6e3b1df7f669ad2fc2c1c
joaquin <mitroncito25@hotmail.com>
2022-10-25 10:12:37 -0500

    AGREGANDO CARRITO AL PROYECTO

---
+++ scripts/productos.js
@@ -0,0 +1,73 @@
+const productos = [{
+        id: 1,
+        foto: "../imagenes/all-in-one.jpg",
+        nombre: "All in One",
+        precio: 1500
+    },
+    {
+        id: 2,
+        foto: "../imagenes/laptop.jpg",
+        nombre: "Laptop lenovo",
+        precio: 1800
+    },
+    {
+        id: 3,
+        foto: "../imagenes/monitor.jpg",
+        nombre: "Monitor Gigabyte",
+        precio: 2000
+    },
+    {
+        id: 4,
+        foto: "../imagenes/parlante.jpg",
+        nombre: "Parlante Logitech",
+        precio: 100
+    },
+    {
+        id: 5,
+        foto: "../imagenes/teclado_mouse.jfif",
+        nombre: "Teclado y Mouse",
+        precio: 80
+    },
+    {
+        id: 6,
+        foto: "../imagenes/disco_solido.jpg",
+        nombre: "Disco solido 480gb",
+        precio: 170
+    },
+    {
+        id: 7,
+        foto: "../imagenes/all-in-one.jpg",
+        nombre: "m.2 480GB",
+        precio: 180
+    },
+    {
+        id: 8,
+        foto: "../imagenes/laptop.jpg",
+        nombre: "Iphone 12 pro max",
+        precio: 1800
+    },
+    {
+        id: 9,
+        foto: "../imagenes/monitor.jpg",
+        nombre: "Fuente para pc",
+        precio: 250
+    },
+    {
+        id: 10,
+        foto: "../imagenes/parlante.jpg",
+        nombre: "Mouse Halion inalambrico",
+        precio: 50
+    },
+    {
+        id: 11,
+        foto: "../imagenes/teclado_mouse.jfif",
+        nombre: "Teclado y Mouse",
+        precio: 80
+    },
+    {
+        id: 12,
+        foto: "../imagenes/disco_solido.jpg",
+        nombre: "Disco solido 480gb",
+        precio: 170
+    },
+];
---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+
+const carrito = [];
+let totalCarrito;
+let contenedor = document.getElementById("misprods");
+
+
+function renderizarProds(){
+    for(const producto of productos){
+        contenedor.innerHTML += `
+            <div class="card col-sm-2">
+                <img src=${producto.foto} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.id}</h5>
+                    <p class="card-text">${producto.nombre}</p>
+                    <p class="card-text">S/. ${producto.precio}</p>
+                    <button id="btn${producto.id}" class="btn btn-primary">Comprar</button>         
+                </div>
+            </div>
+        `;
+    }
+
+    //EVENTOS
+    productos.forEach(producto => {
+        //evento para cada boton
+        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
+            agregarAlCarrito(producto);
+        });
+    })
+}
+
+renderizarProds();
+
+function agregarAlCarrito(productoComprado){
+    carrito.push(productoComprado);
+    console.table(carrito);
+    alert("Producto: "+productoComprado.nombre+" agregado al carrito!");
+    document.getElementById("tablabody").innerHTML += `
+        <tr>
+            <td>${productoComprado.id}</td>
+            <td>${productoComprado.nombre}</td>
+            <td>${productoComprado.precio}</td>
+            <td><button><i class="bi bi-trash3-fill"></i></button> </td>
+        </tr>
+    `;
+    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText="Total a pagar S/.: "+totalCarrito;
+}
+
+
+
